7bb21b07d427cd167a7bf7f25e6d8f260e8090f5
KasaPatrik <kasaip02@gmail.com>
2023-01-30 13:38:22 +0100

    2.0

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const app = express();
 const ObjectId = require("mongodb").ObjectId;
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
 
-const cors = require('cors');
-
-app.use(cors({
-	origin : 'http://localhost:3000',
-	credentials: true,
-	methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-	headers: 'Origin, Pragma, Cache-control, X-Requested-With, Content-Type, Accept, Authorization'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    headers:
+      "Origin, Pragma, Cache-control, X-Requested-With, Content-Type, Accept, Authorization",
+  })
+);
 
 function getClient() {
   let MongoClient = require("mongodb").MongoClient;
   let uri =
     "mongodb://patrik:nfoGU3FTAyZJtxjl@ac-zmyztoo-shard-00-00.dcwwdiy.mongodb.net:27017,ac-zmyztoo-shard-00-01.dcwwdiy.mongodb.net:27017,ac-zmyztoo-shard-00-02.dcwwdiy.mongodb.net:27017/?ssl=true&replicaSet=atlas-hars5n-shard-0&authSource=admin&retryWrites=true&w=majority";
-  return new MongoClient(uri);
+  return new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 function getId(raw) {
@@ -27,6 +32,7 @@ function getId(raw) {
     return "";
   }
 }
+
 
 // get all users
 app.get("/users", (req, res) => {
@@ -40,6 +46,7 @@ app.get("/users", (req, res) => {
     client.close();
   });
 });
+
 
 // add new user
 app.post("/users", bodyParser.json(), (req, res) => {
@@ -64,6 +71,7 @@ app.post("/users", bodyParser.json(), (req, res) => {
   });
 });
 
+
 // delete user
 app.delete("/users/:id", (req, res) => {
   const id = getId(req.params.id);
@@ -84,9 +92,35 @@ app.delete("/users/:id", (req, res) => {
   });
 });
 
-//---------------------------------------------------------------
+
+//----------------------------------------------------------------------
+//get all markers
+app.get("/markers/:id", (req, res) => {
+  const id = getId(req.params.id);
+  if (!id) {
+    res.send({ error: "Invalid id" });
+    return;
+  }
+  const client = getClient();
+  client.connect(async (err, client) => {
+    const collection = client.db("sport_project").collection("users");
+
+    const result = await collection.findOne({ _id: id });
+    res.send(result);
+
+    client.close();
+  });
+});
+
+
 // add new marker to user
-app.post("/marker/:id", bodyParser.json(), (req, res) => {
+app.post("/markers/:id", bodyParser.json(), (req, res) => {
+  const newMarker = {
+    lat: req.body.lat,
+    lng: req.body.lng,
+    sport: req.body.sport,
+    timerange: req.body.timerange,
+  };
   const id = getId(req.params.id);
   if (!id) {
     res.send({ error: "Invalid id" });
@@ -95,17 +129,13 @@ app.post("/marker/:id", bodyParser.json(), (req, res) => {
   const client = getClient();
   client.connect(async (err) => {
     const collection = client.db("sport_project").collection("users");
-    const result = await collection.updateMany(
+    const result = await collection.findOneAndUpdate(
       { _id: id },
-      {
-        $set: {
-          markers: req.body,
-        },
-      },
+      { $push: { markers: newMarker } },
       { returnDocument: "after" }
     );
     if (!result.ok) {
-      res.send({ error: "Not found" });
+      res.send({ error: "Insert Error" });
       return;
     }
     res.send(result.value);
@@ -114,23 +144,35 @@ app.post("/marker/:id", bodyParser.json(), (req, res) => {
 });
 
 
-//get all markers
-app.get("/markers/:id", (req, res) => {
+// delete a marker from user
+app.delete("/markers/:id", bodyParser.json(), (req, res) => {
+  const marker = {
+    lat: req.body.lat,
+    lng: req.body.lng,
+    sport: req.body.sport,
+    timerange: req.body.timerange,
+  };
   const id = getId(req.params.id);
-	if (!id) {	
-		res.send({error: 'Invalid id'});
-		return;
-	}
+  if (!id) {
+    res.send({ error: "Invalid id" });
+    return;
+  }
   const client = getClient();
-  client.connect(async (err, client) => {
+  client.connect(async (err) => {
     const collection = client.db("sport_project").collection("users");
-
-    const markers = await collection.findOne({_id: id});
-    res.send(markers);
-
+    const result = await collection.updateOne(
+      { _id: id },
+      {
+        $pull: {markers: marker}
+      }
+    );
+    if (!result.modifiedCount) {
+      res.send({ error: "Not found" });
+      return;
+    }
+    res.send({ id: id });
     client.close();
   });
 });
-
 
 app.listen(9000);
