cef96e0b9f9c22a8943bd428cc39573adce856df
KasaPatrik <kasaip02@gmail.com>
2023-02-09 12:18:29 +0100

    a

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 app.use(express.json());
 app.use(cors());
-require('dotenv').config();
+require("dotenv").config();
 require("./userDetails");
 
-const JWT_SECRET = process.env.JWT_SECRET
-const mongoUrl = process.env.MONGO_URL
+const JWT_SECRET = process.env.JWT_SECRET;
+const mongoUrl = process.env.MONGO_URL;
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -33,16 +33,16 @@ app.post("/register", async (req, res) => {
   try {
     const oldUser = await User.findOne({ email });
     if (oldUser) {
-      return res.send({ error: "User already exists" });
+      return res.json({ error: "User already exists" });
     }
     await User.create({
       username,
       email,
       password: encryptedPassword,
     });
-    res.send({ status: "ok" });
+    res.json({ status: "ok" });
   } catch (error) {
-    res.send({ status: "error" });
+    res.json({ status: "error" });
   }
 });
 
@@ -56,7 +56,9 @@ app.post("/login", async (req, res) => {
     return res.json({ error: "User not found" });
   }
   if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+    const token = jwt.sign({ email: user.email }, JWT_SECRET,  {
+      expiresIn: "10h",
+    });
 
     if (res.status(201)) {
       return res.json({ status: "ok", data: token });
@@ -67,7 +69,7 @@ app.post("/login", async (req, res) => {
   res.json({ status: "error", error: "Invalid password" });
 });
 
-//------------------------------------------------------------------
+//----------------------------------------------------------------
 // add new marker
 app.post("/add-marker", async (req, res) => {
   const token = req.body.token;
@@ -102,61 +104,21 @@ app.post("/add-marker", async (req, res) => {
     });
     findUser.markers.push(marker);
     findUser.save();
-    res.send({ status: "ok" });
+    res.json({ status: "ok" });
   } catch (error) {
-    res.send({ status: "error" });
-  }
-});
-
-//get user's data
-app.post("/get-userdata", async (req, res) => {
-  const { token } = req.body;
-  try {
-    const user = jwt.verify(token, JWT_SECRET);
-    const useremail = user.email;
-    User.findOne({ email: useremail })
-      .then((data) => {
-        res.send({ status: "ok", data: data });
-      })
-      .catch((error) => {
-        res.send({ status: "error", data: error });
-      });
-  } catch (error) {
-    res.send({ status: "error", error: error });
-  }
-});
-
-//get all data
-app.post("/get-alldata", async (req, res) => {
-  const { token } = req.body;
-  try {
-    const user = jwt.verify(token, JWT_SECRET);
-    if (!user) {
-      res.json({ status: "error", error: "Invalid User" });
-    }
-    User.collection
-      .find()
-      .toArray()
-      .then((data) => {
-        res.send({ status: "ok", data: data });
-      })
-      .catch((error) => {
-        res.send({ status: "error", data: error });
-      });
-  } catch (error) {
-    res.send({ status: "error", error: error });
+    res.json({ status: "error" });
   }
 });
 
 // delete a marker
 app.post("/delete-marker", async (req, res) => {
   const { token } = req.body;
-  const selected = { 
-    lat: req.body.selected.lat, 
+  const selected = {
+    lat: req.body.selected.lat,
     lng: req.body.selected.lng,
     sport: req.body.selected.sport,
-    time: req.body.selected.time
-   };
+    time: req.body.selected.time,
+  };
 
   try {
     const user = jwt.verify(token, JWT_SECRET);
@@ -170,13 +132,71 @@ app.post("/delete-marker", async (req, res) => {
       }
     )
       .then((data) => {
-        res.send({ status: "ok", data: data });
+        res.json({ status: "ok", data: data });
       })
       .catch((error) => {
-        res.send({ status: "error", data: error });
+        res.json({ status: "error", data: error });
       });
   } catch (error) {
-    res.send({ status: "error", message: error });
+    res.json({ status: "error", message: error });
+  }
+});
+
+
+//---------------------------------------------------------------
+//get user's data
+app.post("/get-userdata", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET, (err, res) => {
+      if (err) {
+        return "token expired";
+      }
+      return res;
+    });
+    if (user == "token expired") {
+      return res.json({ status: "error", data: "token expired"})
+    }
+    const useremail = user.email;
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.json({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.json({ status: "error", data: error });
+      });
+  } catch (error) {
+    res.json({ status: "error", error: error });
+  }
+});
+
+//get all users data
+app.post("/get-alldata", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET, (err, res) => {
+      if (err) {
+        return "token expired";
+      }
+      return res;
+    });
+    if (user == "token expired") {
+      return res.json({ status: "error", data: "token expired"})
+    }
+    if (!user) {
+      res.json({ status: "error", error: "Invalid User" });
+    }
+    User.collection
+      .find()
+      .toArray()
+      .then((data) => {
+        res.json({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.json({ status: "error", data: error });
+      });
+  } catch (error) {
+    res.json({ status: "error", error: error });
   }
 });
 
